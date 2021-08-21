07616f77b741ad36dc59ccde4f0d650e2fb50367
Nav jazz <nav9876q@gmail.com>
2021-08-21 12:55:42 +0530

    commiting inventory angular project 1.0

---
+++ src/app/address.ts
@@ -0,0 +1,7 @@
+export class Address {
+
+    addressId: number=0;
+    street: string="";
+    city: string="";
+    pincode:number=0;
+}
---
+++ src/app/dealer-address.ts
@@ -0,0 +1,20 @@
+export class DealerAddress {
+
+    
+    id: number=0;
+    email: string="";
+    fname: string="";
+    lname: string="";
+    password: string="";
+    dob: Date=new Date();
+    phoneNo:string="";
+
+    street:string="";
+    city:string="";
+    pincode:string="";
+    
+    active: boolean=false;
+
+    
+
+}
